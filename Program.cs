9c03936c65437cb3d88e83ab4ffa20cf19def736
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-22 10:33:23 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+// Marvish Chandra
+
+public class famousCheesecakes{
+    string cheesecake1;
+    string cheesecake2;
+    string cheesecake3;
+    string cheesecake4;
+    string cheesecake5;
+    string cheesecake6;
+    string cheesecake7;
+    string cheesecake8;
+    string cheesecake9;
+    string cheesecake10;
+    string cheesecake11;
+    string cheesecake12;
+    string cheesecake13;
+    string cheesecake14;
+    string cheesecake15;
+    string cheesecake16;
+    string cheesecake17;
+    string cheesecake18;
+    string cheesecake19;
+    string cheesecake20;
+    string cheesecake21;
+    string cheesecake22;
+    string cheesecake23;
+    string cheesecake24;
+    string cheesecake25;
+    string cheesecake26;
+    string cheesecake27;
+    string cheesecake28;
+    string cheesecake29;
+    string cheesecake30;
+    string cheesecake31;
+    string cheesecake32;
+    string cheesecake33;
+    string cheesecake34;
+
+    static void signaturecakes(string[] args){
+        famousCheesecakes.cheesecake1 = "Original";
+        famousCheesecakes.cheesecake2 = "Fresh Strawberry";
+        famousCheesecakes.cheesecake3 = "Celebration Cheesecake";
+        famousCheesecakes.cheesecake4 = "Very Cherry Ghiradelli Chocolate Cheesecake";
+        famousCheesecakes.cheesecake5 = "Chocolate Hazelnut Crunch Cheesecake";
+        famousCheesecakes.cheesecake6 = "Cinnabon Cinnamon Swirl Cheesecake";
+        famousCheesecakes.cheesecake7 = "Salted Caramel Cheesecake";
+        famousCheesecakes.cheesecake8 = "Coffee & Cream Chocolate Supreme";
+        famousCheesecakes.cheesecake9 = "OREO Dream Extreme Cheesecake";
+        famousCheesecakes.cheesecake10 = "Toasted Marshmallow S'mores Galore";
+        famousCheesecakes.cheesecake11 = "Lemon Meringue Cheesecake";
+        famousCheesecakes.cheesecake12 = "Adam's Peanut Butter Cup Fudge Ripple";
+        famousCheesecakes.cheesecake13 = "Godiva Chocolate Cheesecake";
+        famousCheesecakes.cheesecake14 = "Ultimate Red Velvet Cake Cheesecake";
+        famousCheesecakes.cheesecake15 = "Reese's Peanut Butter Chocolate Cake Cheesecake";
+        famousCheesecakes.cheesecake16 = "Dulce de Leche Caramel Cheesecake";
+        famousCheesecakes.cheesecake17 = "White Chocolate Raspberry Truffle";
+        famousCheesecakes.cheesecake18 = "Mango Key Lime Cheesecake";
+        famousCheesecakes.cheesecake19 = "Fresh Banana Cream Cheesecake";
+        famousCheesecakes.cheesecake20 = "White Chocolate Caramel Macadamia Nut Cheesecake";
+        famousCheesecakes.cheesecake21 = "Lemon Raspberry Cream Cheesecake";
+        famousCheesecakes.cheesecake22 = "Chocolate Mousse Cheesecake";
+        famousCheesecakes.cheesecake23 = "Chocolate Tuxedo Cream Cheesecake";
+        famousCheesecakes.cheesecake24 = "Hershey's Chocolate Bar Cheesecake";
+        famousCheesecakes.cheesecake25 = "30th Anniversary Chocolate Cake Cheesecake";
+        famousCheesecakes.cheesecake26 = "Vanilla Bean Cheesecake";
+        famousCheesecakes.cheesecake27 = "Tiramisu Cheesecake";
+        famousCheesecakes.cheesecake28 = "Chocolate Chop Cookie Dough Cheesecake";
+        famousCheesecakes.cheesecake29 = "Key Lime Cheesecake";
+        famousCheesecakes.cheesecake30 = "Low Carb Cheesecake";
+        famousCheesecakes.cheesecake31 = "Low Carb Cheesecake with Strawberries";
+        famousCheesecakes.cheesecake32 = "Caramel Pecan Turtle Cheesecake";
+        famousCheesecakes.cheesecake33 = "Craig's Crazy Carrot Cake Cheesecake";
+        famousCheesecakes.cheesecake34 = "Pumpkin & Pumpkin Pecan";
+    }
+    static void cost(string[] args){
+        regularCheesecake = 7.95;
+        largeCheesecake = 8.95;
+    }
+}
